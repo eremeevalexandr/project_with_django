353283f131d26672c24f2dbef9cee723368781f9
Max Chernyshov <chernyshovmaksim56@gmail.com>
2019-06-06 12:06:08 +0500

    add model Person_information and make migrations

--- account/models.py
+++ account/models.py
@@ -3,3 +3,12 @@ from django.db import models
 class Person(models.Model):
     email = models.CharField(max_length=100)
     password = models.CharField(max_length=200)
+
+class Person_information(models.Model):
+    person = models.OneToOneField(
+        Person,
+        on_delete = models.CASCADE,
+        primary_key = True
+    )
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
