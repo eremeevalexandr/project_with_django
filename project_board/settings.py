288755a7a62a0cb4a8f8f700819f40a6f4475746
chernyshovmaksim <chernyshovmaksim56@gmail.com>
2019-06-16 11:36:34 +0500

    restructure project

--- project_board/settings.py
+++ project_board/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'home_page',
-    'account',
+    'apps.home_page',
+    'apps.account',
 ]
 
 MIDDLEWARE = [
