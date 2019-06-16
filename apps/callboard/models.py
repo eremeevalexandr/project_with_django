78c3cab3dc5f2487f0fbee4fadf3f5454b1aa7a9
chernyshovmaksim <chernyshovmaksim56@gmail.com>
2019-06-16 13:17:18 +0500

    add adverts model and she register in admin panel

--- apps/callboard/models.py
+++ apps/callboard/models.py
@@ -2,7 +2,9 @@ from django.db import models
 from mptt.models import MPTTModel, TreeForeignKey
 
 class Category(MPTTModel):
-    name = models.CharField("Название категории", max_length=100, unique=True)
+    """Модель категорий обьявлений"""
+    name = models.CharField(verbose_name = "Название категории", max_length=100, unique=True)
+    slug = models.SlugField(verbose_name = "Алиас", max_length=100, null=True, blank=True)
     parent = TreeForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='children')
 
     def __str__(self):
@@ -14,3 +16,16 @@ class Category(MPTTModel):
     class Meta:
         verbose_name = "Категория"
         verbose_name_plural = "Категории"
+
+
+class Adverts(models.Model):
+    """Модель объявлений"""
+    category = models.ForeignKey(Category, verbose_name="Категория", on_delete=models.CASCADE)
+    name = models.CharField(verbose_name="Имя объявления", max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Объявление"
+        verbose_name_plural = "Объявления"
